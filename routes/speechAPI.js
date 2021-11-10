3c257d519b673eaef01ed5c42bf9fe7d9dfa32f7
gouthamben10 <gouthamsk98@gmail.com>
2021-11-10 13:44:59 +0530

    testing

--- routes/speechAPI.js
+++ routes/speechAPI.js
@@ -2,13 +2,82 @@
     const express = require('express')
     const router =  express.Router()
     const speech = require('@google-cloud/speech');
+    const { createWriteStream } = require("fs");
     const fs = require('fs')
+    const {Storage} =require('@google-cloud/storage');
+    const path = require('path');
     let id ='0'
     let transcription
 
+    
+
     // Creates a client
     const client = new speech.SpeechClient();
+    const gc = new Storage({
+      keyFilename:path.join(__dirname,'../key/bisoft-bucket.json'),
+      projectID: 'bisoft'
+    })
+
+
+    gc.getBuckets().then(e =>console.log(e))
+
+  
     
+      
+     const filesBucket = gc.bucket('bisoft')
+     //upload method 1
+    // async function uploadBucket(data){
+    //   await new Promise(res =>
+    //     fs.createReadStream('./public/sample6.mp3')
+    //       .pipe(
+    //         filesBucket.file('sample6.mp3').createWriteStream({
+    //           resumable: false,
+    //           gzip: true,
+              
+    //         })
+    //       )
+    //       .on("finish", res)
+    //   )
+    // }
+    //upload method 2
+    const bucketname = 'bisoft';
+    const filename = 'audio';
+    async function upbuck(){
+      const data ='https://it.wisc.edu/wp-content/uploads/Google-Cloud-Platform-900x400-1.jpg'
+      const audio = {
+        uri: data,
+      };
+      const res = await gc.bucket(bucketname).upload('https://it.wisc.edu/wp-content/uploads/Google-Cloud-Platform-900x400-1.jpg' ,{
+        destination: filename
+      });
+    // `mediaLink` is the URL for the raw contents of the file.
+    // const url = res[0].metadata.mediaLink;
+
+    // // Need to make the file public before you can access it.
+    // await gc.bucket(bucketname).file(filename).makePublic();
+
+    // // Make a request to the uploaded URL.
+    // const axios = require('axios');
+    // const pkg = await axios.get(url).then(res => res.data);
+    // pkg.name;
+    }
+
+    
+        
+
+
+
+
+
+    // let localReadStream = fs.createReadStream('./public/sample2.mp3');
+    // let remoteWriteStream = filesBucket.file('sample2.mp3').createWriteStream({
+    //   resumable: false,
+    //   gzip:true
+    // });
+    // localReadStream.pipe(remoteWriteStream)
+    // .on('error', function(err) {console.log(err)})
+    // .on('finish', res);
+   //speech rec function 
     async function quickstart() {
       // The path to the remote LINEAR16 file
       const gcsUri = 'gs://bisoft/sample1.mp3';
@@ -66,9 +135,11 @@ router.post("/", function(req, res){
    console.log("this url is "+req.body.url)
    console.log(typeof req.body.url)
    
+   //uploadBucket(req.body.url);
   
    
 })
-
+upbuck();
+   
 module.exports = router
 
