51fed6834c543685ae38641f8728db67bd4ac57d
gouthamben10 <gouthamsk98@gmail.com>
2021-11-15 15:16:15 +0530

    wait component

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router =  express.Router()
 //const p5 = require('p5')
 const fs = require('fs')
-var delayInMilliseconds = 1000; //1 second
+var delayInMilliseconds = 0; //1 second
 
 setTimeout(function() {
   //your code to be executed after 1 second
@@ -10,6 +10,7 @@ setTimeout(function() {
   fs.watch('./geek.txt',(eventType,filename)=>{
     //console.log('File was updated!!');
     readMe = fs.readFileSync('./geek.txt','utf8')
+    readMe=readMe.toLocaleLowerCase();
     console.log(readMe) 
   })
 
@@ -20,19 +21,33 @@ setTimeout(function() {
  // console.log(`write me= ${writeMe}`) 
  console.log(readMe)
 router.get('/', function (req, res, next){
-  
-   if( readMe.search("Hardware")!=-1)
+    
+   if(readMe.search("hardware")!=-1)
        id='2'
-   if( readMe.search("wait")!=-1||readMe.search("delay")!=-1)
-       id='1'
+   if( readMe.search("wait")!=-1||readMe.search("delay")!=-1){
+       let matches = readMe.match(/(\d+)/);
+       let matchtime
+       if(readMe.search("millisecond")!==-1)
+         matchtime = readMe.match(/millisecond/);
+       else if(readMe.search("second")!==-1)
+         matchtime = readMe.match(/second/);
+       console.log("time delay= "+matchtime[0])
+       id ='wait '+matches[0]+' '+matchtime[0]
+       console.log("time delay id = "+id)
+   }
+      
    if( readMe.search("component")!=-1)
        id='3'
-   if( readMe.search("speak_componet4")!=-1)
-       id='0'
+   if( readMe.search("stop")!=-1)
+       id='4'
    if( readMe.search("speak_componet4")!=-1)
        id='1'
-    if(readMe==='')
-        id=-1
+   if( readMe.search("delete")!=-1)
+       id='5'
+    if(readMe.search("start")!=-1||readMe.search("Start")!=-1)
+       id='6'
+   if(readMe==='')
+       id=-1
    res.send(id)
    readMe=''
    fs.writeFileSync('./geek.txt',readMe)
